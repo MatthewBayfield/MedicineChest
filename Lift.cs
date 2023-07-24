20567656a82712cf88c38707691748d47e8f4189
matthew <matth@DESKTOP-NP816OM>
2023-07-24 14:23:25 +0100

    Create Lift class, add constructor, initialise fields. Create lift instance.

---
+++ Lift.cs
@@ -0,0 +1,28 @@
+﻿namespace MedicineChest
+{
+    internal class Lift
+    {
+        public Dictionary<string, int> SelectedFloorCounters = new();
+        public Dictionary<string, int> CalledFloorCounters = new();
+        public int CurrentFloor;
+        public HashSet<int> SelectedFloorsLive = new();
+        public HashSet<int> SelectedFloorsSnapshot = new();
+        public HashSet<int> CalledFloorsLive = new();
+        public HashSet<int> CalledFloorsSnapshot = new();
+
+        public Lift()
+        {
+            foreach (int i in Enumerable.Range(1, 10))
+            {
+                SelectedFloorCounters.Add($"x_{i}", 0);
+            }
+
+            foreach (int i in Enumerable.Range(1, 10))
+            {
+                CalledFloorCounters.Add($"y_{i}", 0);
+            }
+        }
+    }
+
+    
+}
