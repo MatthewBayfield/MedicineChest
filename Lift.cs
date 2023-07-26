0ae834d23dd4a627ff27cead994429111af74a1e
matthew <matth@DESKTOP-NP816OM>
2023-07-26 01:43:37 +0100

    Create UpdateCalledFloors method. Add extra fields
    Created a method callback, that will be called when a lift call occurs,  and updates the called floors and lift caller sets.

--- Lift.cs
+++ Lift.cs
@@ -2,24 +2,38 @@
 {
     internal class Lift
     {
-        public Dictionary<string, int> SelectedFloorCounters = new();
-        public Dictionary<string, int> CalledFloorCounters = new();
+        public Dictionary<string, int> selectedFloorCounters = new();
+        public Dictionary<string, int> calledFloorCounters = new();
         public int CurrentFloor;
-        public HashSet<int> SelectedFloorsLive = new();
-        public HashSet<int> SelectedFloorsSnapshot = new();
-        public HashSet<int> CalledFloorsLive = new();
-        public HashSet<int> CalledFloorsSnapshot = new();
+        public HashSet<int> selectedFloorsLive = new();
+        public HashSet<int> selectedFloorsSnapshot = new();
+        public HashSet<int> calledFloorsLive = new();
+        public HashSet<int> calledFloorsSnapshot = new();
+        public Boolean liftEmpty = true;
+        public HashSet<int> liftCallers = new();
+        public HashSet<int> liftRiders = new();
 
         public Lift()
         {
             foreach (int i in Enumerable.Range(1, 10))
             {
-                SelectedFloorCounters.Add($"x_{i}", 0);
+                selectedFloorCounters.Add($"x_{i}", 0);
             }
 
             foreach (int i in Enumerable.Range(1, 10))
             {
-                CalledFloorCounters.Add($"y_{i}", 0);
+                calledFloorCounters.Add($"y_{i}", 0);
+            }
+        }
+
+        public void UpdateCalledFloors(List<int> callerIDs)
+        {
+            foreach (int call in callerIDs)
+            {
+                liftCallers.Add(call);
+                int calledFloor = Program.liftCallsDict[call][1];
+                calledFloorsLive.Add(calledFloor);
+                Console.WriteLine("Lift called from floor {0} at t = {1}, by callerID = {2}", calledFloor, Program.liftCallsDict[call][2], call);
             }
         }
     }
