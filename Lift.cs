79f28f56264bdd8b9fb89f730b8cd82786a8c632
matthew <matth@DESKTOP-NP816OM>
2023-07-26 18:46:43 +0100

    Create private lift method DisembarkRiders
    Method is responsible for removing riders from the lift when the current stop matches their desination stop.  Also contains a callback that writes to the output CSV file a record detailing the riders journey times.

--- Lift.cs
+++ Lift.cs
@@ -4,14 +4,17 @@
     {
         public Dictionary<string, int> selectedFloorCounters = new();
         public Dictionary<string, int> calledFloorCounters = new();
-        public int CurrentFloor;
+        public int CurrentFloor = 5;
         public HashSet<int> selectedFloorsLive = new();
         public HashSet<int> selectedFloorsSnapshot = new();
         public HashSet<int> calledFloorsLive = new();
         public HashSet<int> calledFloorsSnapshot = new();
-        public Boolean liftEmpty = true;
+        public Boolean liftFull = false;
         public HashSet<int> liftCallers = new();
         public HashSet<int> liftRiders = new();
+        public int averageStopTime = 30;
+        public int liftCapacity = 8;
+        public int liftAdjacentFloorTravelTime = 10;
 
         public Lift()
         {
@@ -36,7 +39,19 @@
                 Console.WriteLine("Lift called from floor {0} at t = {1}, by callerID = {2}", calledFloor, Program.liftCallsDict[call][2], call);
             }
         }
-    }
 
-    
+        private void DisembarkRiders(int timeWhenStopped)
+        {
+            // adding Output CSV entries for departing riders
+            IEnumerable<int> departingRiders =
+            from riderID in liftRiders
+            where Program.liftCallsDict[riderID][1] == CurrentFloor
+            select riderID;
+            Program.WriteToOutputCSV(departingRiders.ToList(), timeWhenStopped);
+            // removing disembarked riders
+            liftRiders.RemoveWhere((int riderID) => Program.liftCallsDict[riderID][1] == CurrentFloor);
+            departingRiders.ToList().ForEach((int riderID) => Console.WriteLine("Rider with callerID = {0}, departing at current floor = {1}, at t = {2}",
+                                                                                riderID, CurrentFloor, timeWhenStopped));
+        }
+    } 
 }
