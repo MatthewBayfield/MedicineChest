8151ffeefede117e680ea44218530e7c58db443c
matthew <matth@DESKTOP-NP816OM>
2023-08-11 01:39:34 +0100

    Add test for UpdateCalledFloors LiftBase method

--- MedicineChest.Tests/LiftBaseTests.cs
+++ MedicineChest.Tests/LiftBaseTests.cs
@@ -34,5 +34,37 @@ namespace MedicineChest.Tests
             value3 = Assert.Contains("y_10", (IDictionary<string, int>)lift.calledFloorCounters);
             Assert.Equal(0, value3);
         }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        public void UpdateCalledFloors_PassedCallerIDList_UpdatesCalledFloorsLiveSet(int ID1, int ID2, int ID3)
+        {
+
+            using (StringWriter consoleOutput = new())
+            {
+                // Arrange
+                Lift lift = new();
+                Program.liftCallsDict.Add(1, new List<int>(new int[] { 2, 1, 5 }));
+                Program.liftCallsDict.Add(2, new List<int>(new int[] { 1, 9, 17 }));
+                Program.liftCallsDict.Add(3, new List<int>(new int[] { 10, 5, 32 }));
+                Console.SetOut(consoleOutput);
+
+                // Act
+                lift.UpdateCalledFloors(new List<int>(new int[] { ID1, ID2, ID3 }));
+
+                // Assert
+                Assert.Equal(3, lift.calledFloorsLive.Count);
+                Assert.Contains(2, lift.calledFloorsLive);
+                Assert.Contains(1, lift.calledFloorsLive);
+                Assert.Contains(10, lift.calledFloorsLive);
+                using (StringReader reader = new(consoleOutput.ToString()))
+                {
+
+                    Assert.Equal($"Lift called from floor 2 at t = 5, by callerID = {ID1}", reader.ReadLine());
+                    Assert.Equal($"Lift called from floor 1 at t = 17, by callerID = {ID2}", reader.ReadLine());
+                    Assert.Equal($"Lift called from floor 10 at t = 32, by callerID = {ID3}", reader.ReadLine());
+                }
+            }
+        }
     }
 }
